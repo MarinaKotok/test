ba4eebc143f764ebddadda5cc51e9240107aca1d
Maryna Kotok <marina_kotok@ukr.net>
2018-08-19 19:21:33 +0300

    Project is created

---
+++ KotokTestProject/src/test/java/KotokTestProject/Login_Test.java
@@ -0,0 +1,11 @@
+package KotokTestProject;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class Login_Test extends ParentTest; {
+@Test
+public void validLogin_TC(){
+        }
+
+        }
---
+++ KotokTestProject/src/test/java/ParentTest/ParentTest.java
@@ -0,0 +1,5 @@
+package ParentTest;
+
+public class ParentTest {
+
+}
